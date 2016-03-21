3db19824fd2282ddf1776770a5115508dc65b65d
Tom Opgenorth <tom@opgenorth.net>
2016-03-21 10:52:48 -0600

    Adding the sample project and some tests.

---
+++ android/BackdoorActivity/App/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace BackdoorActivity
+{
+	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+        [Java.Interop.Export("StartActivityTwo")]
+        public void StartActivityTwo()
+	    {
+            Intent i = new Intent(this, typeof(SecondActivity));
+            StartActivity(i);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.first_activity);
+
+		    FindViewById<Button>(Resource.Id.button1).Click += (sender, args) =>
+		                                                       {
+                                                                   Intent i = new Intent(this, typeof(SecondActivity));
+                                                                   StartActivity(i);
+		                                                       };
+		}
+	}
+}
+
+
---
+++ android/BackdoorActivity/UITests/Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace BackdoorActivity.UITests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp
+                .Android
+                // TODO: Update this path to point to your Android app and uncomment the
+                // code if the app is not included in the solution.
+                //.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
+                .StartApp();
+        }
+
+        AndroidApp app;
+
+        [Test]
+        public void Click_button_for_second_activity()
+        {
+            app.WaitForElement(c => c.Marked("button1"));
+            app.Tap(c => c.Button("button1"));
+
+            app.WaitForElement(c => c.Marked("editText2"));
+            app.EnterText(c => c.TextField("editText2"), "Some text.");
+            app.DismissKeyboard();
+        }
+
+        [Test]
+        public void Start_activity_two()
+        {
+            app.WaitForElement(c => c.Marked("button1"));
+            app.Invoke("StartActivityTwo:");
+
+            app.WaitForElement(c => c.Marked("editText2"));
+            app.EnterText(c => c.TextField("editText2"), "Some text that different.");
+            app.DismissKeyboard();
+        }
+    }
+}
